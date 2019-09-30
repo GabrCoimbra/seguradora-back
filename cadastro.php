876228cced39e40ebc2d6bbb8f1a61b555da6d43
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-30 13:04:32 -0300

    Verificando retorno do cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,8 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+ header('Access-Control-Allow-Origin: *');
+ header("Access-Control-Allow-Credentials: true");
+ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+ header('Access-Control-Max-Age: 1000');
+ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 require_once 'model/cadastro.php';
 
 $json = file_get_contents('php://input');
 $obj  = json_decode($json, true);
+echo $_POST;
 echo json_decode($obj);
