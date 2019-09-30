c936799361c5ba9e7960f7a18c0679647856ac5c
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-30 13:43:52 -0300

    Refazendo o insert

--- model/cadastro.php
+++ model/cadastro.php
@@ -5,7 +5,15 @@ require_once './conn/conexao.php';
 $database = new Database();
 $db = $database->conexao();
     function cadastrar($dados){
-        $sql = 'insert into tb_calculos values('.$dados.')';
+        $sql = 'insert into tb_calculos values(
+            null,
+            '.$dados['nome'].',
+            '.$dados['sexo'].',
+            '.$dados['date'].',
+            '.$dados['marca'].',
+            '.$dados['veiculo'].',
+            1
+            )';
         $envia = $db->query($query);
         return $envia;
     }
