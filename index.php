8862ddf9d4d3aaacc00d13f8fc8b049b4887921a
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-29 21:33:31 -0300

    Json de retorno de calculos

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+require_once 'model/listagem.php';
+
+$dados = array();
+$calculos = listar();
+foreach( $calculos as $i){
+    
+    $i = array(
+        'id' => $i['cd_Calculo'],
+        'nome' => $i['nm_Condutor'],
+        'marca' => $i['ds_Marca'],
+        'veiculo' => $i['ds_Veiculo']
+    );
+    array_push($dados, $i);
+}
+
+ echo  json_encode($dados);
