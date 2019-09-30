db5d8f9f7b6ffacb15388c7fc30564ccdce133e2
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-30 12:18:54 -0300

    Configurando novo banco de dados

--- conn/conexao.php
+++ conn/conexao.php
@@ -3,7 +3,8 @@ class Database
 {
     public function conexao()
     {
-        $conn = new mysqli("mysql.fatecrl.edu.br", "fatecrl05", "seguradora123", "fatecrl05");
+        $conn = new mysqli("mysql.fatecrl.edu.br", "fatecrl05", "seguradora123", "fatecrl05") or die('Error ao se conectar ao servidor: '. mysql_error());
+        ;
         $conn->query("SET NAMES 'utf8'");
         $conn->query('SET character_set_connection=utf8');
         $conn->query('SET character_set_client=utf8');
