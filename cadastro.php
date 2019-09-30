41a9db7cba6f86999f968a3a1904af5f59e62015
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-30 17:58:40 -0300

    Comentando o codigo e recebendo apenas os jsons passados pelo front

--- cadastro.php
+++ cadastro.php
@@ -1,27 +1,22 @@
 <?php
+//Organizando o cabeçalho para requisições json
  header('Access-Control-Allow-Origin: *');
  header("Access-Control-Allow-Credentials: true");
  header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
  header('Access-Control-Max-Age: 1000');
  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-
+//Incluindo a model de cadastro
 require_once 'model/cadastro.php';
-
+//recebendo o json enviado pela API
 $json = file_get_contents('php://input');
-$obj  = json_decode($json, true);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, "http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.dll/veiculo?cod_marca=10");//.$obj['idMarcca']);
-$result = curl_exec($ch);
-curl_close($ch);
-$veiculo = in_array("Ranget", $result);
-// $veiculo = array_search('Ranger', $result['descricao_veiculo']);
-echo $veiculo;
-echo $result;
-$result = json_decode($result, true);
 
-/*
+//Transformando o json em array
+$obj  = json_decode($json, true);
+
+//Enviando os parametros para a função 
 $retorno = cadastrar($obj);
+
+//Verifica se foi cadastrado
 if($retorno){
     echo json_encode('Cadastrado com sucesso');
 }
@@ -29,4 +24,3 @@ else{
     echo json_encode('Não funcionou');
 }
 
-*/
